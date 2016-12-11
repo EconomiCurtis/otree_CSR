179b0342dba7b8ee248c04e72175b838b755d62b
EconomiCurtis <curtisesjunk@gmail.com>
2016-12-11 10:44:29 +0400

    csr ret

---
+++ csr_0_realeffort/models.py
@@ -0,0 +1,328 @@
+# -*- coding: utf-8 -*-
+# <standard imports>
+from __future__ import division
+
+import otree.models
+from otree.db import models
+from otree import widgets
+from otree.common import Currency as c, currency_range, safe_json
+from otree.constants import BaseConstants
+from otree.models import BaseSubsession, BaseGroup, BasePlayer
+
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+import random
+
+# </standard imports>
+
+
+
+author = 'Curtis Kephart'
+
+doc = """
+CSR Experiment
+This experiment ...
+Designed by Chetan Dave and Alicja Reuben.
+First implementation by Curtis Kephart (curtiskephart@gmail.com) 2016.11
+"""
+
+class Constants(BaseConstants):
+    name_in_url = 'csr_realEffort'
+    players_per_group = 4
+    task_timer = 30
+    num_rounds = 200
+
+    reference_texts = [
+		'KJe783yzU3Ca',
+		'l9veLG7stT3P',
+		'CQH7uKzEgNk0',
+		'Fx8l6AbsZuMs',
+		'EmBMxrAy03J1',
+		'bzu52aHa1Qoa',
+		'hWIk3zeIQYy8',
+		'p8Eyf9rExbfq',
+		't1IVvlgzxnKF',
+		'CzU4HQAWYsyD',
+		'YxRhyujFASW3',
+		'MwRTjhzaFAjn',
+		'qf1YvFXH3Lu7',
+		'3tFi2GNPRD3A',
+		'uZo8XO7LYoFy',
+		'yUTg6MDpXcvB',
+		'sGR6IROq027U',
+		'8X78WIlP8daO',
+		'v0PIyCNNU4Vh',
+		'MPETsO8yh691',
+		'Xx7QPUJ0fY7D',
+		'wI01DIlrefVL',
+		'1KsvfrHBeYso',
+		'T6F7j1ChPwYJ',
+		'KUNmRKlwd0Yk',
+		'erLnhgW0KcLp',
+		'R70l5SQ8guz4',
+		'UiVqjH6iKnHi',
+		'hQmonmRjHlyC',
+		'vWlnom8WkCaU',
+		'Eic7OSdDmIOB',
+		'ZLMxBdChnv52',
+		'JVpSqHuMTa69',
+		'hGFHrC2bVPG9',
+		'1bNJUTehHf6S',
+		'm9h4bjfqdg1D',
+		'neC3YySwdy7l',
+		'1YnebhoASSKZ',
+		'C8fNpXHTOm0X',
+		'JoXW7Ha1Rmbp',
+		'3aBNO7u4ripj',
+		'lRqeo2zqcR9V',
+		'YiIO5UPBbfIK',
+		'FtpBw4SniDsD',
+		'UblqBTFPLJUW',
+		'dsRkfSGPuvyX',
+		'jGI5TtfhXrz6',
+		'4QxBRzXwFeXu',
+		'HPxA3xCa0sgz',
+		'iweAsv967BBs',
+		'JxpWBDWmDxAG',
+		'pDRFDttDzN9q',
+		'0V6gq54T9qga',
+		'sgTI0yNShF85',
+		'vexoIvPtncMq',
+		'44CGXygnQ0NV',
+		'TiRRxmNfUQRI',
+		'6XPCx37lZCKD',
+		'XqSTjDv6dqEb',
+		'7x26HZCoTT0n',
+		'2UzDrqnIA26e',
+		'4AxNz3gAGrcl',
+		'pPBFV8E69FCr',
+		'HeOtzDJ7XpZD',
+		'oWtEHrxktbpI',
+		'lS9NKkvY7KnG',
+		'xdUYWA8HDK7x',
+		'9XxdV5SHikWu',
+		'ZdO2e2HhM1Hx',
+		'qvhgeNnGGJbP',
+		'ZyPiIDJe7ZYg',
+		'7NXE8SNHNYCu',
+		'p4TZOCbrDlpz',
+		'jvyc3wljT7jo',
+		'cAO83uOjEIrN',
+		'F8El8ueLBU5V',
+		'ZAhtrzONppLG',
+		'lSfbuzgH4QKQ',
+		'DaoctzvTAvf7',
+		'9A1xkOeNZhFb',
+		'xxuUnAZxbsTa',
+		'ZBG02ZyRjrPW',
+		'yXnV2KQh5iAq',
+		'ZyZEKfP4r3oR',
+		'hOydsUgtp6gG',
+		'wJ7cEYeXkgSD',
+		'Cpi7S6ttweoZ',
+		'6CUq5kpZS9Ri',
+		'rwWqmGlCrhxd',
+		'RXd8MyGKe53S',
+		'GQExSI5n3j1u',
+		'l8YiQUCblTeY',
+		'pLfZ1ggYdf9h',
+		'ddVAVbf1tyQm',
+		'0B8TP5IVzXxO',
+		'6qYU3iEGUrci',
+		'Te1Ne2s1gNrv',
+		'RAAXuUn4fZ7Z',
+		'R3kMthN5YWFe',
+		'3cXKfldMd5Yu',
+		'4p6GUgOfff8D',
+		'nvUStLglmaKJ',
+		'N2wBcTNrpzSz',
+		'NXSNwlmK6BiB',
+		'HyBTbvPOXgAa',
+		'AY0u0tt5NRlQ',
+		'LSNb50vbEpqY',
+		'VAt42hAF5tyO',
+		'0bp1WYwjZNiL',
+		'30sJtQWIVWvl',
+		'FD6vUGgWoFj4',
+		'HTWy8Ov4dlTn',
+		'ucx2nB3jK8rG',
+		'KSZSTwXM6oR8',
+		'AXEAIspX2Qs6',
+		'z4RbD41nC01l',
+		'PW32ZRSzbBNQ',
+		'GT4PzXYQEJkN',
+		'W8ysLWLfAprE',
+		'CphpRAuHfhW1',
+		'7pgi9N6zbHJN',
+		'GnVG3APRzmEN',
+		'4wjcvMQd9Dv5',
+		'Usi02VLQemsJ',
+		'6ClGbSYi8z4l',
+		'SuTQMR1BkkCj',
+		'2zUuSrTb8QH8',
+		'ZlrtehBDqvm6',
+		'dI5jSvkGJD4K',
+		'efjT5bcST6yX',
+		'hBUQXANajqQp',
+		'Q1CGCm8olGyt',
+		'peRtMSvSgw8M',
+		'fMwPoD7n6R17',
+		'3iLKNUhF4cUt',
+		'6yg67oYHzCat',
+		'YYpS1GM52tSa',
+		'M3Nb8H0Odvnp',
+		'qsCo1bAGrOau',
+		'AtkJdPaExOup',
+		'krErPxX4IijN',
+		'9MDF31Ssv0gc',
+		'DuBK0XYAYz8J',
+		'tCaOA3oW5PTV',
+		'doRGWqk3UOwt',
+		'S9InKmJCZXYA',
+		'kakLlGTHTCbN',
+		'CGK03uGbI7wo',
+		'xLUb9IvB9n21',
+		'dMWZNtCDOpSS',
+		'UpWHoFbSDpT6',
+		'FFlGYLih0421',
+		'DkbmLOYglnjf',
+		'zofDaeQTgHEO',
+		'PjriVQtiV1dd',
+		'NRNY4ZsJRXx3',
+		'usrVGzkvPIXL',
+		'WvBSWTJiAHKP',
+		'QxeGYTzC2UZi',
+		'6EDWSKtCF3SN',
+		'VVLfq7pUko9z',
+		'wwsS58MHX7ap',
+		'F19C0cLgBxAs',
+		'aKaSIGHN6B78',
+		'ntDDnqsyKaIa',
+		'cB6FsHIx1dVp',
+		'HK92BzuLDWcW',
+		'SnAaHKBPi6lg',
+		'E2LEA4XZQ3UJ',
+		'hCgrTTLJTxHf',
+		'hYpoNJebVg7g',
+		'Jcurz0zNqugY',
+		'LWtvvp6cvnqS',
+		'nfexLqFV876a',
+		'HHIZE4yU1Ugn',
+		'nqDRgDhwpKJF',
+		'BTaFdc2r2lZe',
+		'UiDqUrjEWbMN',
+		'GxE13sUuzGUo',
+		'crU51ejBJwji',
+		'f9NQMYYWihEW',
+		'Ft8MQMUaVqhS',
+		'wLeuZ8f2v29t',
+		'B2tiv55Bf1VC',
+		'8sF8F10GRZmk',
+		'gpTLLM6xaqg9',
+		'D1MxKEDK60LX',
+		'pLqe8RFfIEZa',
+		'Ia1hEiETdrSk',
+		'IWR1vWUuJX5C',
+		'kq5sjIB8r8ei',
+		'MApwOSDkfnnD',
+		'W79e9Wgrzgmb',
+		'GYQKoMVE1RSL',
+		'4bAXIJbiv2fv',
+		'TCR5mGp1ZLRV',
+		'NaTvHfbNbXry',
+		'j0LRF5UAaY1L',
+		'x16oN55m4LIW',
+		'bBYUnY3ppKTA',
+		'VBE9lx9UGu1i',
+		'Dnhu8lVyr6aA',
+		'yfV2At1jeTtU',
+		'4dItV2RnDGgJ',
+		'eukPzLsp0XCU',
+		'6qzg2h8ezjNu',
+		'wke1cSU9prgV',
+		'WUAh3CNJmKGf',
+		'YXthklBEH3tG',
+		'D3AYPUPfC5Pe',
+		'U3Z9woy9wXjo',
+		'3xsSak6CuOx1',
+		'fYL0WUgc8qRZ',
+		'epiOMsJcYZLA',
+		'cLXhTFRBj92K',
+		'SFwlPMmXLnrD',
+		'AuJjiZ0hr5kS',
+		'iWI5SDjHdWdc'
+	]
+
+
+
+class Subsession(BaseSubsession):
+	def before_session_starts(self):
+
+		# how long is the real effort task time? 
+		# refer to settings.py settings. 
+		for p in self.get_players():
+		    if 'ret_time' in self.session.config:
+		        p.ret_timer = self.session.config['ret_time']
+		    else:
+		        p.ret_timer = Constants.task_timer
+
+
+class Group(BaseGroup):
+	pass
+
+
+
+class Player(BasePlayer):
+
+
+	ret_timer = models.PositiveIntegerField(
+	    doc="""The length of the real effort task timer."""
+	)
+
+	user_text = models.CharField(
+		doc="user's transcribed text")
+	is_correct = models.BooleanField(
+		doc="did the user get the task correct?")
+	final_score = models.IntegerField(
+		doc="player's total score up to this round")
+
+
+	def set_final_score(self):
+		correct_cnt = 0
+		for p in self.in_all_rounds():
+			if p.payoff != None: 
+				correct_cnt = correct_cnt + p.payoff
+
+		if correct_cnt == None: 
+			self.final_score = 40
+		elif correct_cnt <= 10:
+			self.final_score = 40
+		elif correct_cnt == 11: 
+			self.final_score = 42
+		elif correct_cnt == 12: 
+			self.final_score = 44
+		elif correct_cnt == 13: 
+			self.final_score = 46
+		elif correct_cnt == 14: 
+			self.final_score = 48
+		elif correct_cnt == 15: 
+			self.final_score = 50
+		elif correct_cnt == 16: 
+			self.final_score = 52
+		elif correct_cnt == 17: 
+			self.final_score = 54
+		elif correct_cnt == 18: 
+			self.final_score = 56
+		elif correct_cnt == 19: 
+			self.final_score = 58
+		elif correct_cnt > 19: 
+			self.final_score = 60
+		else:
+			self.final_score = 60
+
+
+
+
+
---
+++ csr_0_realeffort/views.py
@@ -0,0 +1,174 @@
+from . import models
+from ._builtin import Page, WaitPage
+from otree.api import Currency as c, currency_range
+from .models import Constants
+from django.conf import settings
+import time
+
+
+class Instructions1(Page):
+    timeout_seconds = 60
+
+    def is_displayed(self):
+        return self.round_number == 1
+
+
+class Instructions2(Page):
+    def is_displayed(self):
+        return self.round_number == 2
+
+    def before_next_page(self):
+        if ('start_time' not in self.participant.vars):
+            self.participant.vars['start_time'] = time.time()
+            self.participant.vars['end_time'] = time.time() + self.player.ret_timer
+
+class TranscribeTask(Page):   
+
+    form_model = models.Player
+    form_fields = ['user_text']
+
+    def is_displayed(self):
+        if "end_time" not in self.participant.vars:
+            self.participant.vars['end_time'] = time.time() + 995
+
+        return ((self.round_number > 2) & (time.time() < self.participant.vars['end_time']))
+
+    def vars_for_template(self):
+
+        # For Page Timer
+        #get current time
+        current_time = (time.time())
+        # calc how long we've been here
+        time_expended =  current_time - self.participant.vars['start_time']
+
+        # For Page "Number of Correct Words"
+        total_correct = 0
+        for p in self.player.in_all_rounds():
+            if p.payoff != None: 
+                total_correct += p.payoff
+
+        # set up messgaes in transcription task
+        if self.player.in_previous_rounds()[-1].final_score == None: #on very first task
+            correct_last_round = "<br>"
+            final_score = 40
+        else: #all subsequent tasks
+            final_score = int(self.player.in_previous_rounds()[-1].final_score)
+            if self.player.in_previous_rounds()[-1].is_correct:
+                correct_last_round = "Your last guess was <font color='green'>correct</font>"
+            else: 
+                correct_last_round = "Your last guess was <font color='red'>incorrect</font>"
+        
+
+    
+
+
+
+        # Page variables
+        return {
+            'end_time': self.participant.vars['end_time'],
+            'tiemdf': self.participant.vars['end_time'] - current_time,
+            'current_time': current_time,
+            'time_limit':self.player.ret_timer,
+            'init_time': self.participant.vars['start_time'],
+            'time_expended': time_expended,
+            'correct_last_round': correct_last_round,
+            'total_correct': total_correct,
+            'final_score':final_score,
+            'round_count':(self.round_number - 1),
+            'reference_text': Constants.reference_texts[self.round_number - 1],
+            'debug': settings.DEBUG,
+        }
+
+
+
+    def before_next_page(self):
+
+        # find time at button click
+        final_time = time.time()
+        end_of_timer =  self.participant.vars['start_time'] + self.player.ret_timer
+
+        # update player payoffs
+        if (end_of_timer > final_time):
+            if (Constants.reference_texts[self.round_number - 1] == self.player.user_text):
+            	self.player.is_correct = True
+            	self.player.payoff = 1
+            else: 
+            	self.player.is_correct = False
+    	        self.player.payoff = c(0)
+
+        self.player.set_final_score()       
+
+class ResultsWaitPage(WaitPage):
+    def is_displayed(self):
+        return self.round_number == 100
+    def after_all_players_arrive(self):
+        pass
+        # for p in self.group.get_players():
+        #     p.set_payoff()
+
+class TaskResults(Page):
+    def is_displayed(self):
+        return self.round_number == 101
+
+    def vars_for_template(self):
+
+
+        ##### Get Own Score #####################################################
+        total_correct = 0
+        for p in self.player.in_all_rounds():
+            if p.payoff != None: 
+                total_correct += p.payoff
+
+        # get final score, by going through all rounds, and pulling out the max observed, ignoring all the Nones. 
+        total_payoff = max(x.final_score for x in self.player.in_previous_rounds() if x.final_score is not None)
+
+        ######### Get other members of group's scores ##########################
+        op_scores = []
+        for op in self.player.get_others_in_group():
+            op_final_score = max(x.final_score for x in op.in_previous_rounds() if x.final_score is not None)
+            op_scores.append(op_final_score)
+
+        ######### Get other members of group's scores ##########################
+        all_ret_scores = []
+        for p in self.player.get_others_in_group():
+            p_final_score = max(x.final_score for x in p.in_previous_rounds() if x.final_score is not None)
+            all_ret_scores.append(op_final_score)
+
+        ##### save all variables ###############################################
+
+        self.participant.vars['ret_correct'] = int(total_correct)
+        self.participant.vars['ret_score']   = total_payoff # own final score
+        self.participant.vars['op_scores']   = op_scores #other group member scores
+        self.participant.vars['all_ret_scores']   = all_ret_scores #all ret scores in group
+
+
+        # only keep obs if YourEntry player_sum, is not None. 
+        table_rows = []
+        task_num = 0
+        for prev_player in self.player.in_all_rounds():
+            if prev_player.user_text != None:
+                task_num += 1
+                row = {
+                    'round_number': task_num,
+                    'real_text': Constants.reference_texts[prev_player.round_number - 1],
+                    'player_text': prev_player.user_text,
+                   	'is_correct':prev_player.is_correct,
+                    'total_score': prev_player.final_score,
+                }
+                table_rows.append(row)
+
+        return {
+        'total_correct':int(total_correct),
+        'table_rows': table_rows,
+        'total_score':total_payoff,
+        'op_scores':op_scores
+        }
+
+
+page_sequence = [
+    Instructions1, 
+    Instructions2,
+    TranscribeTask,
+    ResultsWaitPage,
+    TaskResults,
+    ]
